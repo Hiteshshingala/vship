a23db5df42e150ea9944f880b45d5e222101d358
shingalahitesh <shingalahitesh2@gmail.com>
2023-08-02 11:34:58 +0530

    - added dev mode server start for development.  - added new apis.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # vship
+# For Development
+1) install nodemon with global command(https://stackoverflow.com/questions/45544726/how-to-use-nodemon-with-express-js-while-npm-start).
+2) npm run devStart
--- routes/marine.js
+++ routes/marine.js
@@ -21,4 +21,40 @@ router.get('/priority', function(req, res, next) {
   res.status(status).send(payload);
 });
 
+router.get('/order-type', function(req, res, next) {
+  logger.log("info", 'get order type called');
+  const {status, payload} = responseService.handleSuccess(200, 'successfully get order type', []);
+  res.status(status).send(payload);
+});
+
+router.get('/departments', function(req, res, next) {
+  logger.log("info", 'get departments called');
+  const {status, payload} = responseService.handleSuccess(200, 'successfully get order departments', []);
+  res.status(status).send(payload);
+});
+
+router.get('/spare-part-details', function(req, res, next) {
+  logger.log("info", 'get spare part details called');
+  const {status, payload} = responseService.handleSuccess(200, 'successfully get order spare part details', []);
+  res.status(status).send(payload);
+});
+
+router.post('/add-part-against-component', function(req, res, next) {
+    logger.log("info", 'post add part against component');
+    const {status, payload} = responseService.handleSuccess(200, 'successfully added part against component', []);
+    res.status(status).send(payload);
+});
+
+router.post('/add-part-to-master', function(req, res, next) {
+    logger.log("info", 'post add part to master');
+    const {status, payload} = responseService.handleSuccess(200, 'successfully added part to master', []);
+    res.status(status).send(payload);
+});
+
+router.post('/add-component', function(req, res, next) {
+    logger.log("info", 'post add component');
+    const {status, payload} = responseService.handleSuccess(200, 'successfully add component', []);
+    res.status(status).send(payload);
+});
+
 module.exports = router;
